5e200c44555d1171d1eaf34cd5b235c2420f4f36
joaofelipear <cs57gkjh8v@privaterelay.appleid.com>
2026-08-29 00:57:01 -0300

    Rename sw[1].js to sw.js

--- sw[1].js
+++
@@ -1,1 +0,0 @@
-self.addEventListener("install",e=>e.waitUntil(caches.open("jf-clima-pro-v3").then(c=>c.addAll(["./","./index.html","./manifest.json","./logo.png"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))))
