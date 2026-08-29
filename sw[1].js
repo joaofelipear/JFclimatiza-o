4ef93ec5a1708be70cc99c18e36aeed832c53332
joaofelipear <cs57gkjh8v@privaterelay.appleid.com>
2026-08-29 00:24:22 -0300

    Rename sw[1].js to sw.js

--- sw[1].js
+++
@@ -1,1 +0,0 @@
-self.addEventListener("install",e=>e.waitUntil(caches.open("climageria-logo-v1").then(c=>c.addAll(["./","./index.html","./manifest.json","./logo.png"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))))
